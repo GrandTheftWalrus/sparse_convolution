6b7fe5478bc7a4ec80e91278e9050b95158595a3
Vaughn Janes <pie-ducky@hotmail.com>
2024-11-20 01:55:57 -0800

    Some cleanup

--- tests/test_sparse_toeplitz.py
+++ tests/test_sparse_toeplitz.py
@@ -19,7 +19,7 @@ def test_sparse_toeplitz():
     # Warning: Testing the accuracy scales horribly because
     # it checks the output against an explicit matrix
     # multiplication.
-    TEST_ACCURACY = True
+    TEST_ACCURACY = False
 
     stt = shapes_to_try = [
         # (1,1000, 1,1),
@@ -52,16 +52,16 @@ def test_sparse_toeplitz():
         (80,80, 2, 2),
         (90,90, 2, 2),
         (100,100, 2, 2),
-        # (1000,1000, 3, 3),
-        # (2000,2000, 3, 3),
-        # (3000,3000, 3, 3),
-        # (4000,4000, 3, 3),
-        # (5000,5000, 3, 3),
-        # (6000,6000, 3, 3),
-        # (7000,7000, 3, 3),
-        # (8000,8000, 3, 3),
-        # (9000,9000, 3, 3),
-        # (10000,10000, 3, 3),
+        (1000,1000, 3, 3),
+        (2000,2000, 3, 3),
+        (3000,3000, 3, 3),
+        (4000,4000, 3, 3),
+        (5000,5000, 3, 3),
+        (6000,6000, 3, 3),
+        (7000,7000, 3, 3),
+        (8000,8000, 3, 3),
+        (9000,9000, 3, 3),
+        (10000,10000, 3, 3),
         # (3,3, 2, 2),
         # (512,512, 32, 32),
         # (10000,10000, 1, 1),
@@ -106,7 +106,7 @@ def test_sparse_toeplitz():
             for shape in stt:
                 for mode in modes:
                     print(f'\nTesting shape: {shape}, batch_size: {batch_size}, density: {density}, mode: {mode}')
-                    
+                    time_start = time.time()
                     # Generate random testing matrices
                     input_matrices_shape = (batch_size, shape[0], shape[1])
                     input_matrices = np.zeros(input_matrices_shape)
@@ -121,47 +121,35 @@ def test_sparse_toeplitz():
                     kernel = np.random.rand(*k_shape)
 
                     # Get the indices of empty rows of B
-                    nonzero_detection_start = time.time()
                     nonzero_B_rows = np.where(B.any(axis=1))[0].tolist()
-                    nonzero_detection_time = time.time() - nonzero_detection_start
-                    print(f'nonzero_detection_time: {nonzero_detection_time:8.2f}s')
 
                     # Form the bare minimum double Toeplitz
                     init_start = time.time()
                     dt_helper = DoubleToeplitzHelper(shape, kernel)
                     num_elements = len(nonzero_B_rows) * k_shape[0] * k_shape[1]
-                    rows_nd = np.empty(num_elements, dtype=int)
-                    cols_nd = np.empty(num_elements, dtype=int)
-                    data_nd = np.empty(num_elements, dtype=float)
-                    total_nonzero_row_detection_time = 0
+                    rows = np.empty(num_elements, dtype=int)
+                    cols = np.empty(num_elements, dtype=int)
+                    data = np.empty(num_elements, dtype=float)
                     total_value_calculation_time = 0
                     current_index = 0
                     for n, col in enumerate(nonzero_B_rows):
-                        nz_row_detect_start = time.time()
                         nonzero_rows = dt_helper.get_nonzero_rows(col)
                         num_rows = len(nonzero_rows)
-                        total_nonzero_row_detection_time += time.time() - nz_row_detect_start
                         value_calc_start = time.time()
                         # Assign values to the preallocated arrays
-                        rows_nd[current_index:current_index + num_rows] = nonzero_rows
-                        cols_nd[current_index:current_index + num_rows] = col
-                        data_nd[current_index:current_index + num_rows] = [
+                        rows[current_index:current_index + num_rows] = nonzero_rows
+                        cols[current_index:current_index + num_rows] = col
+                        data[current_index:current_index + num_rows] = [
                             dt_helper.get_value(row, col) for row in nonzero_rows
                         ]
                         current_index += num_rows
                         total_value_calculation_time += time.time() - value_calc_start
 
-                    csr_creation_start = time.time()
-                    DT = scipy.sparse.csr_matrix((data_nd, (rows_nd, cols_nd)), shape=dt_helper.shape)
-                    csr_creation_time = time.time() - csr_creation_start
+                    DT = scipy.sparse.csr_matrix((data, (rows, cols)), shape=dt_helper.shape)
                     init_time = time.time() - init_start
-                    print(f'total_nonzero_row_detection_time: {total_nonzero_row_detection_time:8.2f}s')
-                    print(f'total_value_calculation_time: {total_value_calculation_time:8.2f}s')
-                    print(f'csr_creation_time: {csr_creation_time:8.2f}s')
+                    print(f'value calc time:\t{total_value_calculation_time:8.2f}s')
 
-                    mul_start = time.time()
                     out_uncropped = DT @ B
-                    mul_time = time.time() - mul_start
 
                     # Unvectorize output
                     so = size_output_array = ((shape[0] + k_shape[0] - 1), (kernel.shape[1] + shape[1] -1))  ## 'size out' is the size of the output array
@@ -192,6 +180,7 @@ def test_sparse_toeplitz():
 
                     # Crop the output
                     out = out_uncropped[:, t:b, l:r]
+                    time_taken = time.time() - time_start
 
                     if TEST_ACCURACY:
                         # Compute expected output using convolve2d for each batch
@@ -217,14 +206,12 @@ def test_sparse_toeplitz():
                                 f"Got:\n{out[i]}\n"
                             )
                         print(f'init_time:       {init_time:8.3f}s'
-                            f'\nmul_time:        {mul_time:8.3f}s'
-                            f'\ntotal:           {init_time + mul_time:8.3f}s'
+                            f'\ntotal:           {time_taken:8.3f}s'
                             f'\nmul_conv2d_time: {mul_conv2d_time:8.3f}s')
-                        print(f'Speedup:         {mul_conv2d_time / (mul_time + init_time):8.3f}x')
+                        print(f'Speedup:         {mul_conv2d_time / time_taken:8.3f}x')
                     else:
                         print(f'init_time:       {init_time:8.2f}s'
-                            f'\nmul_time:        {mul_time:8.2f}s'
-                            f'\ntotal:           {init_time + mul_time:8.2f}s')
+                            f'\ntotal:           {time_taken:8.2f}s')
 
 if __name__ == '__main__':
     test_sparse_toeplitz()
